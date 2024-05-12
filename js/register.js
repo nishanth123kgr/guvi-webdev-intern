6d243a590681d491c311b37fbe7adff99d99d664
Nishanth R <69259993+nishanth123kgr@users.noreply.github.com>
2024-05-12 23:28:59 +0530

    addded animation

--- js/register.js
+++ js/register.js
@@ -71,3 +71,17 @@ function validatePassword() {
 
 
 passwordInput.addEventListener('input', validatePassword);
+
+setInterval(() => {
+    let waves = document.querySelector('.waves');
+
+    let waveList = document.querySelectorAll('.wave');
+    if (waveList.length > 0) {
+        let lastWave = waveList[waveList.length - 1];
+        waves.removeChild(lastWave);
+        waves.insertBefore(lastWave, waves.firstChild);
+    }
+
+    console.log(document.querySelectorAll('.wave').length);
+
+}, 1000);
